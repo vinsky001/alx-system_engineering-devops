7abec165f2a5a09655339cd1e09f23cbab55e4af
vinsky001 <ekibet544@gmail.com>
2023-04-28 11:28:22 -0700

    C program that creates 5 zombie processes.

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -1,5 +1,52 @@
-#!/usr/bin/env bash
-# Parses Apache log files in list format.
-#   Displays the IP and HTTP status code.
+/*
+ * File: 102-zombie.c
+ * Auth:  Elvis Vinsky
+ */
 
-awk '{print $1 " " $9}' apache-access.log
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+/**
+ * infinite_while - Run an infinite while loop.
+ *
+ * Return: Always 0.
+ */
+int infinite_while(void)
+{
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
+}
+
+/**
+ * main - Creates five zombie processes.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	pid_t pid;
+	char count = 0;
+
+	while (count < 5)
+	{
+		pid = fork();
+		if (pid > 0)
+		{
+			printf("Zombie process created, PID: %d\n", pid);
+			sleep(1);
+			count++;
+		}
+		else
+			exit(0);
+	}
+
+	infinite_while();
+
+	return (EXIT_SUCCESS);
+}
