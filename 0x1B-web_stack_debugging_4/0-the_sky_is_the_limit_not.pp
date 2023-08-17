2ffb6d26a697f2bbdb6748764b36228561cb5631
vinsky001 <ekibet544@gmail.com>
2023-08-17 18:56:27 +0000

    Fixed Indentation

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,5 @@
 # Fixing Stack
-exec {'file limit':
-  onlyif   => 'test -e /etc/default/nginx',
-  command  => 'sed -i "5s/[0-9]\+/$( ulimit -n )/" /etc/default/nginx; service nginx restart',
-  provider => shell,
-} 
+exec { 'change nginx limit':
+    command  => 'sudo sed -i "s/15/4096/g" /etc/default/nginx; sudo service nginx restart',
+    provider => shell,
+}
